38cdf2422711a0a500c9d6dc30d5031d228daaf8
Seiwa-Edu <kuma.0131@icloud.com>
2021-02-13 15:08:56 +0900

    [add] add the simpale component

--- App.js
+++ App.js
@@ -7,13 +7,10 @@ export default function App() {
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View
-      style={{
-        height: 100,
-        width: "100%",
-        borderColor: "gray",
-        borderWidth: 1
-      }
-      }></View>
+      style={styles.itemContainer}>
+        <View style={styles.leftContainer}/>
+        <View style={styles.rightContainer}/>
+      </View>
     </View>
   );
 }
@@ -25,4 +22,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+    height: 100,
+    width: "100%",
+    borderColor: "gray",
+    borderWidth: 1,
+    flexDirection: "row"
+  }, 
+  leftContainer: {
+    backgroundColor: "red",
+    width: 100
+
+  }, 
+  rightContainer: {
+    backgroundColor: "blue",
+    flex: 1
+  }
 });
